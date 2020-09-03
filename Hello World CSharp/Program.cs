9613a135913625e634389258fede32929a0cbfe0
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-03 09:02:49 -0400

    test

---
+++ Hello World CSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hello_World_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //part 1.1
+            Console.WriteLine("Gaven");
+
+            //part 1.2
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+            
+            //part 1.3
+            //asks how old you are then prints it
+            Console.WriteLine("How old are you?");
+            string input2 = Console.ReadLine();
+            Console.WriteLine($"You are {input2} years old!");
+
+            //part 2.1
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Henry");
+
+            //test
+        }
+    }
+}
